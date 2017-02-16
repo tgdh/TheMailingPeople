0ce6d9c5ef94ec2accd4001b0293d58b551954a4
Paulius Putna <up614961@myport.ac.uk>
2017-02-16 17:28:18 +0000

    Search form and ezSearch macro

---
+++ TMP.Core/Utility/TextHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace TMP.Core.Utility
+{
+    public class TextHelper
+    {
+        public static string TruncateAtWord(string text, int length, bool keepFullWordAtEnd = true)
+        {
+            const string ellipsis = "...";
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            if (text.Length < length)
+            {
+                return text;
+            }
+
+            text = text.Substring(0, length);
+
+            if (keepFullWordAtEnd)
+            {
+                text = text.Substring(0, text.LastIndexOf(' '));
+                text = text + ellipsis;
+            }
+
+            return text.Replace(",...", "...").Replace(";...", "...");
+        }
+    }
+
+}
